903322d58b440494741123ee65ace6598211aca7
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-15 22:48:50 -0700

    35. Suscribiendo otro Observador y modificando el Stream reactivo

--- src/main/java/com/rodarte/webflux/models/controllers/ProductoController.java
+++ src/main/java/com/rodarte/webflux/models/controllers/ProductoController.java
@@ -2,6 +2,8 @@ package com.rodarte.webflux.models.controllers;
 
 import com.rodarte.webflux.models.dao.ProductoDao;
 import com.rodarte.webflux.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +16,23 @@ public class ProductoController {
     @Autowired
     private ProductoDao productoDao;
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductoController.class);
+
     @GetMapping({ "/", "/listar" })
     private String listar(Model model) {
 
-        Flux<Producto> productos = productoDao.findAll();
+        Flux<Producto> productos =
+            productoDao
+                .findAll()
+                .map(producto -> {
+                    producto.setNombre(producto.getNombre().toUpperCase());
+                    return producto;
+                });
+
+        productos
+            .subscribe(
+                producto -> logger.info(producto.getNombre())
+            );
 
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado de productos");
