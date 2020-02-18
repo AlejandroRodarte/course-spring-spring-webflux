021b4e01209eea7308f1a8ac9a2158e58f73b028
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-18 11:49:04 -0700

    48. Agregando método eliminar en el Controlador

--- src/main/java/com/rodarte/webflux/models/controllers/ProductoController.java
+++ src/main/java/com/rodarte/webflux/models/controllers/ProductoController.java
@@ -135,6 +135,32 @@ public class ProductoController {
 
     }
 
+    @GetMapping("/eliminar/{id}")
+    public Mono<String> eliminar(@PathVariable String id) {
+
+        return
+            productoService
+                .findById(id)
+                .defaultIfEmpty(new Producto())
+                .flatMap(producto -> {
+
+                    if (producto.getId() == null) {
+                        return Mono.error(new InterruptedException("No existe el producto a eliminar"));
+                    }
+
+                    return Mono.just(producto);
+
+                })
+                .doOnNext(producto -> {
+                    logger.info("Eliminando producto: " + producto.getNombre());
+                    logger.info("Eliminando producto Id: " + producto.getId());
+                })
+                .flatMap(producto -> productoService.delete(producto))
+                .thenReturn("redirect:/listar?success=producto+eliminado+con+exito")
+                .onErrorResume(ex -> Mono.just("redirect:/listar?error=no+existe+el+producto+a+eliminar"));
+
+    }
+
     @GetMapping("/listar-data-driver")
     public String listarDataDriver(Model model) {
 
